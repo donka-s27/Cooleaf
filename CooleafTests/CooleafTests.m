954c56e0a6b6760c8581146c973260aca60b2e18
Donka Stoyanov <donka-s27@users.noreply.github.com>
2016-10-19 16:09:50 +0300

    Add files via upload

---
+++ CooleafTests/CooleafTests.m
@@ -0,0 +1,34 @@
+//
+//  CooleafTests.m
+//  CooleafTests
+//
+//  Created by Bazyli Zygan on 14.12.2013.
+//  Copyright (c) 2013 Nova Project. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CooleafTests : XCTestCase
+
+@end
+
+@implementation CooleafTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
